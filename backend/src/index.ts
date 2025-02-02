2067b4b84621a8a8e0335b59a22f5a94a2bd99c7
ReddyyZ <arthurc.oli.whitehat@gmail.com>
2025-02-02 10:49:53 -0300

    build: initial backend setup

---
+++ backend/src/index.ts
@@ -0,0 +1,17 @@
+import express, { Express } from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import routes from "./routes";
+
+dotenv.config();
+
+const app: Express = express();
+const port = process.env.PORT || 3000;
+
+app.use(cors());
+app.use(express.json());
+app.use(routes);
+
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}/`)
+});
---
+++ backend/src/routes.ts
@@ -0,0 +1,9 @@
+import express, { Request, Response } from "express";
+
+const routes = express.Router();
+
+routes.get("/", (req: Request, res: Response) => {
+  res.send("Hello World!")
+});
+
+export default routes;
