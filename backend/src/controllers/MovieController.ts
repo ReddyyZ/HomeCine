6c25e6ccccf45c10d395d91bc1289026b50b598f
ReddyyZ <arthurc.oli.whitehat@gmail.com>
2025-02-03 13:55:44 -0300

    feat: add update and delete methods

--- backend/src/controllers/MovieController.ts
+++ backend/src/controllers/MovieController.ts
@@ -1,5 +1,5 @@
 import { Request, Response } from "express";
-import { deleteMovieById, findAllEpisodesByMovieId, findAllEpisodesFromSeason, findAllMovies, findAllSeries, findMovieById, getNumberOfSeasons } from "../functions/MovieFuncs";
+import { addMovieProps, deleteEpisodeById, deleteMovieById, findAllEpisodesByMovieId, findAllEpisodesFromSeason, findAllMovies, findAllSeries, findMovieById, getNumberOfSeasons, updateMovieById } from "../functions/MovieFuncs";
 
 export async function getMovies(req: Request, res: Response) {
   const movies = await findAllMovies();
@@ -11,6 +11,7 @@ export async function getMovie(req: Request, res: Response) {
   const { movieId } = req.params;
   if (!movieId) {
     res.status(400).json({ error: "Missing movieId" });
+    return;
   };
 
   const movie = await findMovieById(Number(movieId));
@@ -22,11 +23,12 @@ export async function deleteMovie(req: Request, res: Response) {
   const { movieId } = req.params;
   if (!movieId) {
     res.status(400).json({ error: "Missing movieId" });
+    return;
   };
 
   const result = await deleteMovieById(Number(movieId));
 
-  res.json(result);
+  res.status(result.errorCode ? result.errorCode : 200).json(result);
 };
 
 export async function getSeries(req: Request, res: Response) {
@@ -39,6 +41,7 @@ export async function getSeasonsFromMovie(req: Request, res: Response) {
   const { movieId } = req.params;
   if (!movieId) {
     res.status(400).json({ error: "Missing movieId" });
+    return;
   };
 
   const seasons = await getNumberOfSeasons(Number(movieId));
@@ -50,6 +53,7 @@ export async function getEpisodesFromSeason(req: Request, res: Response) {
   const { movieId, season } = req.params;
   if (!movieId || !season) {
     res.status(400).json({ error: "Missing movieId or season" });
+    return;
   };
 
   const episodes = await findAllEpisodesFromSeason(Number(movieId), Number(season));
@@ -61,9 +65,35 @@ export async function getAllEpisodesByMovieId(req: Request, res: Response) {
   const { movieId } = req.params;
   if (!movieId) {
     res.status(400).json({ error: "Missing movieId" });
+    return;
   };
 
   const episodes = await findAllEpisodesByMovieId(Number(movieId));
 
   res.json(episodes);
 };
+
+export async function updateMovie(req: Request, res: Response) {
+  const { movieId } = req.params;
+  const props: Partial<addMovieProps> = req.body;
+  if (!movieId || !props) {
+    res.status(400).json({ error: "Missing movieId or props" });
+    return;
+  };
+
+  const result = await updateMovieById(Number(movieId), props);
+
+  res.status(result.errorCode ? result.errorCode : 200).json(result);
+}
+
+export async function deleteEpisode(req: Request, res: Response) {
+  const { episodeId } = req.params;
+  if (!episodeId) {
+    res.status(400).json({ error: "Missing id" });
+    return;
+  };
+
+  const result = await deleteEpisodeById(Number(episodeId));
+  
+  res.status(result.errorCode ? result.errorCode : 200).json(result);
+}
--- backend/src/routes/MovieRoutes.ts
+++ backend/src/routes/MovieRoutes.ts
@@ -8,6 +8,9 @@ router.get("/movies/:movieId", MovieController.getMovie);
 router.get("/movies/:movieId/seasons", MovieController.getSeasonsFromMovie);
 router.get("/movies/:movieId/season/:season", MovieController.getEpisodesFromSeason);
 
+router.post("/movies/:movieId/update", MovieController.updateMovie);
+
 router.delete("/movies/:movieId", MovieController.deleteMovie);
+router.delete("/movies/:movieId/episode/:episodeId", MovieController.deleteEpisode);
 
 export default router;
