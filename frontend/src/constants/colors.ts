8b09f2522e7574c5f6261ffa0ac7d7b47b35ecc6
ReddyyZ <arthurc.oli.whitehat@gmail.com>
2025-02-06 15:39:49 -0300

    feat(frontend): add colors constant

---
+++ frontend/src/constants/colors.ts
@@ -0,0 +1,10 @@
+export default {
+  primary: "#E50914",
+  secondary: "#FFA726",
+  primaryBg: "#121212",
+  secondaryBg: "#1E1E1E",
+  cardBg: "#252525",
+  text: "#E0E0E0",
+  mutedText: "#B0B0B0",
+  border: "#3A3A3A",
+};
