4ce6369d86e4eba2d3820c5b9bc41a3715ce7380
ReddyyZ <arthurc.oli.whitehat@gmail.com>
2025-02-06 15:42:12 -0300

    feat(frontend): implement AuthProvider

---
+++ frontend/src/contexts/AuthProvider.tsx
@@ -0,0 +1,46 @@
+import React, { useContext, createContext } from "react";
+import { login, loginProps } from "../services/apiClient";
+
+interface ProviderProps {
+  user: string | null;
+  login(data: loginProps): void;
+  logout(): void;
+}
+
+const AuthContext = createContext<ProviderProps>({
+  user: null,
+  login: () => {},
+  logout: () => {},
+});
+
+const AuthProvider = ({ children }: { children: React.ReactNode }) => {
+  const user = localStorage.getItem("user");
+
+  const loginFunc = async (data: loginProps) => {
+    const result = await login(data);
+    if (!result.data) {
+      return;
+    }
+    if (result.data.success) {
+      localStorage.setItem("user", result.data.user);
+    } else {
+      console.log(result.data.error);
+    }
+  };
+
+  const logout = () => {
+    localStorage.removeItem("user");
+  };
+
+  const value = {
+    user,
+    login: loginFunc,
+    logout,
+  };
+
+  return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
+};
+
+export default AuthProvider;
+
+export const useAuth = () => useContext(AuthContext);
