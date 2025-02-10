76db167e820ca3d99ff9bcfab4b373e0c559871b
ReddyyZ <arthurc.oli.whitehat@gmail.com>
2025-02-10 11:01:23 -0300

    feat(frontend): check if user session is valid before redirecting

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,12 +2,40 @@ import "./App.css";
 import { Route, Routes } from "react-router-dom";
 import Login from "./pages/Auth/Login";
 import Register from "./pages/Auth/Register";
+import { useAuth } from "./contexts/AuthProvider";
+import Home from "./pages/Home";
+import { jwtDecode } from "jwt-decode";
 
-function App() {
+function AuthRoutes() {
   return (
     <Routes>
       <Route path="/" element={<Login />} />
       <Route path="/register" element={<Register />} />
+    </Routes>
+  );
+}
+
+function App() {
+  const auth = useAuth();
+  const checkUser = () => {
+    if (!auth.user) return false;
+    const jwt = jwtDecode(auth.user);
+
+    if (!jwt.exp) return false;
+
+    if (jwt.exp * 1000 < Date.now()) {
+      auth.logout();
+      return false;
+    } else {
+      return true;
+    }
+  };
+
+  return !checkUser() ? (
+    <AuthRoutes />
+  ) : (
+    <Routes>
+      <Route path="/" element={<Home />} />
     </Routes>
   );
 }
