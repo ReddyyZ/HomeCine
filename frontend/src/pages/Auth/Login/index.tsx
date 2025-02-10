c6dbbb4aff6a00e7d9cd3fab885e626e3dc2efc4
ReddyyZ <arthurc.oli.whitehat@gmail.com>
2025-02-10 14:01:23 -0300

    style(frontend): fix conflicts

--- frontend/src/pages/Auth/Login/index.tsx
+++ frontend/src/pages/Auth/Login/index.tsx
@@ -54,84 +54,87 @@ const Login: React.FC = () => {
     }
 
     setLoading(false);
+    window.location.reload();
   };
 
   return (
-    <div className="max-w-lg">
-      {result.message && (
-        <Alert
-          type={result.success ? "success" : "error"}
-          style={{
-            marginBottom: 8,
-          }}
-        >
-          {result.message}
-        </Alert>
-      )}
-      <form
-        onSubmit={login}
-        style={{
-          backgroundColor: colors.secondaryBg,
-        }}
-        className="pl-8 pr-8 pt-[52px] pb-[52px] rounded-sm relative"
-      >
-        {loading && (
-          <div
-            className="flex justify-center items-center rounded-sm mb-2"
+    <div className="loginDiv">
+      <div className="max-w-lg w-96">
+        {result.message && (
+          <Alert
+            type={result.success ? "success" : "error"}
             style={{
-              position: "absolute",
-              top: "50%",
-              left: "50%",
-              transform: "translate(-50%, -50%)",
-              backgroundColor: "rgba(0, 0, 0, 0.2)",
-              backdropFilter: "blur(5px)",
-              width: "100%",
-              height: "100%",
+              marginBottom: 8,
             }}
           >
-            <LoadingIndicator />
-          </div>
+            {result.message}
+          </Alert>
         )}
-        <Logo size="medium" />
-        <p className="text-2xl font-bold mb-2 mt-8">Welcome Back</p>
-        <p>Sign in to continue to HomeCine</p>
+        <form
+          onSubmit={login}
+          style={{
+            backgroundColor: colors.secondaryBg,
+          }}
+          className="pl-8 pr-8 pt-[52px] pb-[52px] rounded-sm relative"
+        >
+          {loading && (
+            <div
+              className="flex justify-center items-center rounded-sm mb-2"
+              style={{
+                position: "absolute",
+                top: "50%",
+                left: "50%",
+                transform: "translate(-50%, -50%)",
+                backgroundColor: "rgba(0, 0, 0, 0.2)",
+                backdropFilter: "blur(5px)",
+                width: "100%",
+                height: "100%",
+              }}
+            >
+              <LoadingIndicator />
+            </div>
+          )}
+          <Logo size="medium" />
+          <p className="text-2xl font-bold mb-2 mt-8">Welcome Back</p>
+          <p>Sign in to continue to HomeCine</p>
 
-        <div className="mt-6 mb-6">
-          <div className="w-full mb-4">
-            <label htmlFor="email" className="mb-2">
-              Email
-            </label>
-            <Input
-              placeholder="Email"
-              type="email"
-              id="email"
-              value={email}
-              onChangeText={setEmail}
-            />
+          <div className="mt-6 mb-6">
+            <div className="w-full mb-4">
+              <label htmlFor="email" className="mb-2">
+                Email
+              </label>
+              <Input
+                placeholder="Email"
+                type="email"
+                id="email"
+                value={email}
+                onChangeText={setEmail}
+              />
+            </div>
+            <div className="w-full">
+              <label htmlFor="password" className="mb-2">
+                Password
+              </label>
+              <Input
+                placeholder="Password"
+                type="password"
+                id="password"
+                value={password}
+                onChangeText={setPassword}
+              />
+            </div>
           </div>
-          <div className="w-full">
-            <label htmlFor="password" className="mb-2">
-              Password
-            </label>
-            <Input
-              placeholder="Password"
-              type="password"
-              id="password"
-              value={password}
-              onChangeText={setPassword}
-            />
-          </div>
-        </div>
 
-        <Button type="submit">Sign In</Button>
+          <Button type="submit">Sign In</Button>
 
-        <p className="text-center mt-6">
-          Don't have an account?{" "}
-          <Link style={{ color: colors.secondary }} to="/register">
-            Sign up
-          </Link>
-        </p>
-      </form>
+          <p className="text-center mt-6">
+            Don't have an account?{" "}
+            <Link style={{ color: colors.secondary }} to="/register">
+              Sign up
+            </Link>
+          </p>
+        </form>
+      </div>
     </div>
   );
 };
--- frontend/src/pages/Auth/Register/index.tsx
+++ frontend/src/pages/Auth/Register/index.tsx
@@ -5,7 +5,7 @@ import Input from "../../../components/Input";
 import Button from "../../../components/Button";
 import colors from "../../../constants/colors";
 import { useAuth } from "../../../contexts/AuthProvider";
-import { Link, useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 import Alert from "../../../components/Alert";
 import LoadingIndicator from "../../../components/LoadingIndicator";
 
@@ -18,7 +18,6 @@ const Register: React.FC = () => {
   const [name, setName] = React.useState("");
   const [email, setEmail] = React.useState("");
   const [password, setPassword] = React.useState("");
-  const navigate = useNavigate();
   const [result, setResult] = React.useState({} as Result);
   const [loading, setLoading] = React.useState(false);
 
@@ -56,100 +55,99 @@ const Register: React.FC = () => {
     }
 
     setLoading(false);
-    // navigate("/");
+    window.location.reload();
   };
 
   return (
-    <div className="max-w-lg">
-      {result.message && (
-        <Alert
-          type={result.success ? "success" : "error"}
-          style={{
-            marginBottom: 8,
-          }}
-        >
-          {result.message}
-        </Alert>
-      )}
-      <form
-        onSubmit={login}
-        style={{
-          backgroundColor: colors.secondaryBg,
-        }}
-        className="pl-8 pr-8 pt-[52px] pb-[52px] rounded-sm relative"
-      >
-        {loading && (
-          <div
-            className="flex justify-center items-center rounded-sm mb-2"
+    <div className="loginDiv">
+      <div className="max-w-lg">
+        {result.message && (
+          <Alert
+            type={result.success ? "success" : "error"}
             style={{
-              position: "absolute",
-              top: "50%",
-              left: "50%",
-              transform: "translate(-50%, -50%)",
-              backgroundColor: "rgba(0, 0, 0, 0.2)",
-              backdropFilter: "blur(5px)",
-              width: "100%",
-              height: "100%",
+              marginBottom: 8,
             }}
           >
-            <LoadingIndicator />
-          </div>
+            {result.message}
+          </Alert>
         )}
-        <Logo size="medium" />
-        <p className="text-2xl font-bold mb-2 mt-8">Sign Up</p>
-        <p>Create an account to start enjoying HomeCine</p>
-
-        <div className="mt-6 mb-6">
-          <div className="w-full mb-4">
-            <label htmlFor="email" className="mb-2">
-              Name
-            </label>
-            <Input
-              placeholder="Name"
-              type="text"
-              id="name"
-              onChange={(e) => {
-                setName(String(e.target.value));
+        <form
+          onSubmit={login}
+          style={{
+            backgroundColor: colors.secondaryBg,
+          }}
+          className="pl-8 pr-8 pt-[52px] pb-[52px] rounded-sm relative"
+        >
+          {loading && (
+            <div
+              className="flex justify-center items-center rounded-sm mb-2"
+              style={{
+                position: "absolute",
+                top: "50%",
+                left: "50%",
+                transform: "translate(-50%, -50%)",
+                backgroundColor: "rgba(0, 0, 0, 0.2)",
+                backdropFilter: "blur(5px)",
+                width: "100%",
+                height: "100%",
               }}
-            />
-          </div>
-          <div className="w-full mb-4">
-            <label htmlFor="email" className="mb-2">
-              Email
-            </label>
-            <Input
-              placeholder="Email"
-              type="email"
-              id="email"
-              onChange={(e) => {
-                setEmail(String(e.target.value));
-              }}
-            />
-          </div>
-          <div className="w-full">
-            <label htmlFor="password" className="mb-2">
-              Password
-            </label>
-            <Input
-              placeholder="Password"
-              type="password"
-              id="password"
-              onChange={(e) => {
-                setPassword(String(e.target.value));
-              }}
-            />
-          </div>
-        </div>
+            >
+              <LoadingIndicator />
+            </div>
+          )}
+          <Logo size="medium" />
+          <p className="text-2xl font-bold mb-2 mt-8">Sign Up</p>
+          <p>Create an account to start enjoying HomeCine</p>
 
-        <Button type="submit">Sign Up</Button>
+          <div className="mt-6 mb-6">
+            <div className="w-full mb-4">
+              <label htmlFor="email" className="mb-2">
+                Name
+              </label>
+              <Input
+                placeholder="Name"
+                type="text"
+                id="name"
+                value={name}
+                onChangeText={setName}
+              />
+            </div>
+            <div className="w-full mb-4">
+              <label htmlFor="email" className="mb-2">
+                Email
+              </label>
+              <Input
+                placeholder="Email"
+                type="email"
+                id="email"
+                value={email}
+                onChangeText={setEmail}
+              />
+            </div>
+            <div className="w-full">
+              <label htmlFor="password" className="mb-2">
+                Password
+              </label>
+              <Input
+                placeholder="Password"
+                type="password"
+                id="password"
+                value={password}
+                onChangeText={setPassword}
+              />
+            </div>
+          </div>
 
-        <p className="text-center mt-6">
-          Already have an account?{" "}
-          <Link style={{ color: colors.secondary }} to="/">
-            Sign in
-          </Link>
-        </p>
-      </form>
+          <Button type="submit">Sign Up</Button>
+
+          <p className="text-center mt-6">
+            Already have an account?{" "}
+            <Link style={{ color: colors.secondary }} to="/">
+              Sign in
+            </Link>
+          </p>
+        </form>
+      </div>
     </div>
   );
 };
