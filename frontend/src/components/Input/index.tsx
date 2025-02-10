46c2833993533e4a7bc4fb600534804b582bd592
ReddyyZ <arthurc.oli.whitehat@gmail.com>
2025-02-10 10:48:50 -0300

    feat(frontend): add search/set password visible buttons on Input component

--- frontend/src/components/Input/index.tsx
+++ frontend/src/components/Input/index.tsx
@@ -1,21 +1,93 @@
-import { ClassAttributes, InputHTMLAttributes } from "react";
-import { JSX } from "react/jsx-runtime";
+import { InputHTMLAttributes, useState } from "react";
 import colors from "../../constants/colors";
 import "./styles.css";
+import { IoSearch, IoEye, IoEyeOff } from "react-icons/io5";
+import { IconBaseProps } from "react-icons";
 
-export default function Input(
-  props: JSX.IntrinsicAttributes &
-    ClassAttributes<HTMLInputElement> &
-    InputHTMLAttributes<HTMLInputElement>,
-) {
+export interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
+  value: string;
+  onChangeText: (value: string) => void;
+  type?: "text" | "password" | "email" | "search";
+}
+
+interface PasswordInputIcon extends IconBaseProps {
+  visible: boolean;
+  setVisible: (visible: boolean) => void;
+}
+
+const PasswordInputIcon = ({
+  visible,
+  setVisible,
+  ...props
+}: PasswordInputIcon) => {
   return (
-    <input
+    <button onClick={() => setVisible(!visible)}>
+      {visible ? <IoEye {...props} /> : <IoEyeOff {...props} />}
+    </button>
+  );
+};
+
+export default function Input({
+  type,
+  onChangeText,
+  value,
+  ...props
+}: InputProps) {
+  const [isPasswordVisible, setPasswordVisible] = useState(false);
+  const PasswordIcon = (props: IconBaseProps) => (
+    <PasswordInputIcon
+      visible={isPasswordVisible}
+      setVisible={(visible) => setPasswordVisible(visible)}
+      {...props}
+    />
+  );
+  const Icon =
+    type === "search"
+      ? IoSearch
+      : type === "password"
+        ? value.length
+          ? PasswordIcon
+          : null
+        : null;
+
+  return (
+    <div
       style={{
         backgroundColor: colors.cardBg,
         color: colors.text,
       }}
       className="input"
-      {...props}
-    />
+    >
+      <input
+        style={{
+          backgroundColor: colors.cardBg,
+          color: colors.text,
+        }}
+        // className="input"
+        type={
+          type === "password"
+            ? isPasswordVisible
+              ? "text"
+              : "password"
+            : type === "email"
+              ? "email"
+              : "text"
+        }
+        onChange={(e) => onChangeText(e.target.value)}
+        {...props}
+      />
+      <div className="flex items-center justify-center p-2">
+        {Icon && (
+          <Icon
+            size={24}
+            fill={colors.text}
+            style={{
+              opacity: value.length ? 1 : 0.6,
+              transition: "opacity 0.2s",
+            }}
+          />
+        )}
+      </div>
+    </div>
   );
 }
