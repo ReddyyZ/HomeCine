20ef275478d10332c6d60867039015231b50e13c
ReddyyZ <arthurc.oli.whitehat@gmail.com>
2025-02-19 13:25:46 -0300

    style(frontend): Modal component: fix height overflow

--- frontend/src/components/Modal/index.tsx
+++ frontend/src/components/Modal/index.tsx
@@ -1,9 +1,9 @@
-import { useEffect } from "react";
+import { JSX, useEffect } from "react";
 /* import {} from 'react-icons/io5'; */
 
 type ModalProps = {
   visible: boolean;
-  children: () => JSX.Element | React.Node;
+  children: (() => JSX.Element) | JSX.Element;
   onDismiss: () => void;
   containerClassName?: string;
   containerStyle?: React.CSSProperties;
@@ -26,42 +26,44 @@ export default function Modal({
 }: ModalProps) {
   useEffect(() => {
     const detectESCKey = (e) => {
-      if (event.key === "Escape") {
+      if (e.key === "Escape") {
         onDismiss();
       }
     };
 
-    document.addEventListener("keydown", detectESCKey);
-
-    return () => document.removeEventListener("keydown", detectESCKey);
+    return () => {
+      document.removeEventListener("keydown", detectESCKey);
+    };
   });
 
   return (
     visible && (
       <div
         className={
-          "appear-animation p-4" +
+          "appear-animation fixed top-0 right-0 bottom-0 left-0 overflow-auto p-4" +
           (containerClassName ? ` ${containerClassName}` : "")
         }
         style={containerStyle}
       >
+        <div className="relative h-full w-full">
+          <div
+            className={
+              "bg-secondaryBg scrollbar scrollbar-thumb-[#252525] scrollbar-track-[#1E1E1E] absolute top-1/2 left-1/2 z-30 h-full max-h-160 w-full max-w-5xl -translate-x-1/2 -translate-y-1/2 transform overflow-y-auto rounded-lg p-4" +
+              (modalClassName ? ` ${modalClassName}` : "")
+            }
+            style={modalStyle}
+          >
+            {children}
+          </div>
+        </div>
         <div
           className={
-            "absolute top-0 left-0 h-full w-full bg-[#000000b3]" +
+            "fixed top-0 left-0 h-full w-full bg-[#000000b3]" +
             (bgClassName ? ` ${bgClassName}` : "")
           }
           onClick={onDismiss}
           style={bgStyle}
         />
-        <div
-          className={
-            "bg-secondaryBg absolute top-1/2 left-1/2 h-full max-h-160 w-full max-w-5xl -translate-x-1/2 -translate-y-1/2 transform rounded-lg p-4" +
-            (modalClassName ? ` ${modalClassName}` : "")
-          }
-          style={modalStyle}
-        >
-          {children}
-        </div>
       </div>
     )
   );
