d5185a77dffb15cc3c41de389094f8eedf4d6f2c
ReddyyZ <arthurc.oli.whitehat@gmail.com>
2025-02-27 15:24:32 -0300

    feat(frontend): LoadingView component: add children prop

--- frontend/src/components/LoadingView/index.tsx
+++ frontend/src/components/LoadingView/index.tsx
@@ -2,12 +2,17 @@ import LoadingIndicator from "../LoadingIndicator";
 
 type LoadingViewProps = {
   size?: "small" | "medium" | "large";
+  children?: React.ReactNode;
 };
 
-export default function LoadingView({ size = "medium" }: LoadingViewProps) {
+export default function LoadingView({
+  size = "medium",
+  children,
+}: LoadingViewProps) {
   return (
-    <div className="appear-animation bg-black-opacity-100 absolute z-40 flex h-full w-full items-center justify-center rounded-sm backdrop-blur-sm">
+    <div className="appear-animation bg-black-opacity-100 absolute z-40 flex h-full w-full flex-col items-center justify-center rounded-sm backdrop-blur-sm">
       <LoadingIndicator size={size} />
+      {children}
     </div>
   );
 }
