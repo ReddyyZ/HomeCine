e50866c1c545d266d7d58c457f22c8690e0860ed
ReddyyZ <arthurc.oli.whitehat@gmail.com>
2025-02-27 13:43:11 -0300

    fix(backend): requests were being rejected because of payload size limit

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -4,6 +4,7 @@ import cors from "cors";
 import routes from "./routes";
 import "./db/sequilize";
 import path from "path";
+import bodyParser from "body-parser";
 
 dotenv.config();
 
@@ -11,8 +12,10 @@ const app: Express = express();
 const port = process.env.PORT || 3000;
 
 app.use(cors());
+app.use(bodyParser.json({ limit: "50mb" }));
+app.use(bodyParser.urlencoded({ limit: "50mb", extended: true }));
 app.use(express.json());
-app.use('/media', express.static(path.join(__dirname,"../media/thumbnails")));
+app.use("/media", express.static(path.join(__dirname, "../media/thumbnails")));
 app.use(routes);
 
 app.listen(port, () => {
